e11afe52d442060da7119b94a049335264d5460f
m17603048828 <675974990@qq.com>
2018-09-28 21:18:12 +0800

    更新一下下

--- 003---CI/003---CITests/_03___CITests.m
+++ 003---CI/003---CITests/_03___CITests.m
@@ -22,6 +22,7 @@
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     NSLog(@"hello word");
+    NSLog(@"hello CI");
 }
 
 - (void)testPerformanceExample {
